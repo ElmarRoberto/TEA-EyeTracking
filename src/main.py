f381f9c068d481d1125dc90a1cfcedfbc6baf07c
ElmarRoberto <elmarcfilho@gmail.com>
2019-05-29 20:33:35 -0300

    :bulb: Add eye recognition

--- src/main.py
+++ src/main.py
@@ -20,6 +20,21 @@ faces = face_cascade.detectMultiScale(gray_picture, 1.3, 5)
 for (x,y,w,h) in faces:
     cv2.rectangle(img, (x,y), (x+w,y+h), (255, 255, 0), 2)
 
+gray_face = gray_picture[y:y+h, x:x+w] # cut the gray face frame out
+face = img[y:y+h, x:x+w] # cut the face frame out
+eyes = eye_cascade.detectMultiScale(gray_face)
+
+for (ex,ey,ew,eh) in eyes: 
+    cv2.rectangle(face,(ex,ey),(ex+ew,ey+eh),(0,225,255),2)
+
+
+def detect_eyes(img, img_gray, classifier):
+  coords = cascade.detectMultiScale(img_gray, 1.3, 5)# detect eyes
+  height = np.size(image, 0) # get face frame height
+  for (x, y, w, h) in coords:
+    if y+h > height/2: # pass if the eye is at the bottom
+      pass
+
 # Shows a window with the image
 cv2.imshow('my first image', img)
 cv2.waitKey(0) # press any key
